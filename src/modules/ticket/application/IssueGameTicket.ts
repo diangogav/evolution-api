24e5c6643572d569bf2ccc1278fa29345a3968b2
DiangoGav <diango.code@gmail.com>
2026-06-16 18:29:13 -0400

    fix: adjust formatting in IssueGameTicket constructor

--- src/modules/ticket/application/IssueGameTicket.ts
+++ src/modules/ticket/application/IssueGameTicket.ts
@@ -2,7 +2,7 @@ import { GameTicket } from "../domain/GameTicket";
 import { RankedTicketRepository } from "../domain/RankedTicketRepository";
 
 export class IssueGameTicket {
-	constructor(private readonly repository: RankedTicketRepository) {}
+	constructor(private readonly repository: RankedTicketRepository) { }
 
 	async issue({ userId }: { userId: string }): Promise<{ ticket: string }> {
 		const ticket = GameTicket.generate();
